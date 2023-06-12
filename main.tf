c1e2c1d8b7a122b7f18fc9ece54d2f659d302d79
Diksha <76660222+diksha1159@users.noreply.github.com>
2023-06-12 09:58:40 +0000

    modified main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "azurerm_virtual_network" "example" {
 }
 
 # Create App Service Plan
-resource "azurerm_service_plan" "example" {
+resource "azurerm_app_service_plan" "example" {
   name                = var.app_service_plan_name
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
@@ -30,12 +30,28 @@ resource "azurerm_app_service" "example" {
   name                 = "${var.web_app_name}-${count.index}"
   location             = azurerm_resource_group.example.location
   resource_group_name  = azurerm_resource_group.example.name
-  app_service_plan_id  = azurerm_service_plan.example.id
+  app_service_plan_id  = azurerm_app_service_plan.example.id
 
   site_config {
     dotnet_framework_version = "v4.0"
     scm_type                 = "LocalGit"
   }
+}
+
+# Create Public IP
+resource "azurerm_public_ip" "example" {
+  name                = "example-public-ip"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+}
+
+# Create Subnet
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create Application Gateway
@@ -57,5 +73,37 @@ resource "azurerm_application_gateway" "example" {
 
   backend_address_pool {
     name = "backend_address_pool_name"
+  }
+
+  backend_http_settings {
+    name                  = "backend_http_settings_name"
+    port                  = 80
+    protocol              = "Http"
+    cookie_based_affinity = "Disabled"
+  }
+
+  frontend_port {
+    name = "frontend_port_name"
+    port = 80
+  }
+
+  gateway_ip_configuration {
+    name      = "gateway_ip_configuration_name"
+    subnet_id = azurerm_subnet.example.id
+  }
+
+  http_listener {
+    name                           = "http_listener_name"
+    frontend_ip_configuration_name = "frontend_ip_configuration_name"
+    frontend_port_name             = "frontend_port_name"
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                  = "request_routing_rule_name"
+    rule_type             = "Basic"
+    http_listener_name    = "http_listener_name"
+    backend_address_pool_name    = "backend_address_pool_name"
+    backend_http_settings_name   = "backend_http_settings_name"
   }
 }
